d83507ac165209f84bfaa9a7fbf3e87053cc0cbb
mercari-komamiya <tmp-h-komamiya@mercari.com>
2022-12-21 23:59:27 +0900

    feat: create updateUser() endpoint

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,20 @@
+import { Body, Controller, Get, Patch, Req, UseGuards } from '@nestjs/common';
 import { Controller } from '@nestjs/common';
+import { UserService } from './user.service';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { User } from '@prisma/client';
 
 @Controller('user')
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
 export class UserController {}
+
+  @Patch()
+  updateUser(
+    @Req() req: Request,
+    @Body() dto: UpdateUserDto,
+  ): Promise<Omit<User, 'hashPassword'>> {
+    return this.userService.updateUser(req.user.id, dto);
+  }
+}
